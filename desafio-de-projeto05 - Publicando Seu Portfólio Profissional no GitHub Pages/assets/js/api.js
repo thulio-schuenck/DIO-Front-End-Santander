407ccc2960760d231a3c467b4ed31a6f34ada7b4
Thulio Schuenck <thulio.schuenck@gmail.com>
2025-10-11 20:30:52 -0300

    Update api.js

--- desafio-de-projeto05 - Publicando Seu Portfólio Profissional no GitHub Pages/assets/js/api.js
+++ desafio-de-projeto05 - Publicando Seu Portfólio Profissional no GitHub Pages/assets/js/api.js
@@ -1,13 +1,7 @@
 
-/*async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/thulio-schuenck/DIO-Front-End-Santander/refs/heads/main/desafio-de-projeto05%20-%20Publicando%20Seu%20Portf%C3%B3lio%20Profissional%20no%20GitHub%20Pages/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
-}*/
-
-async function fetchProfileData() {
-    const url ='https://raw.githubusercontent.com/thulio-schuenck/DIO-Front-End-Santander/refs/heads/main/desafio-de-projeto05%20-%20Publicando%20Seu%20Portf%C3%B3lio%20Profissional%20no%20GitHub%20Pages/data/profile.json'
-    const fetching = await fetch(url) 
-    return await fetching.json()
 }
