fed3a068a857950ed9da3e88887c498d1ca9bac3
Thulio Schuenck <thulio.schuenck@gmail.com>
2025-10-11 20:22:18 -0300

    Desafio de projeto
    Inclusão dos dados json de Thulio

---
+++ desafio-de-projeto05 - Publicando Seu Portfólio Profissional no GitHub Pages/assets/js/api.js
@@ -0,0 +1,13 @@
+
+/*async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const response = await fetch(url)
+    const profileData = await response.json()
+    return profileData
+}*/
+
+async function fetchProfileData() {
+    const url ='https://raw.githubusercontent.com/thulio-schuenck/js-developer-portfolio/refs/heads/main/data/profile.json'
+    const fetching = await fetch(url) 
+    return await fetching.json()
+}
