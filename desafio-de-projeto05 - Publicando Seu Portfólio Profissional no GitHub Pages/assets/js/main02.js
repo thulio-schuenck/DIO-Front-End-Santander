1c56bf7dd62f36b56a707ee5beb05643d19afa22
Thulio Schuenck <thulio.schuenck@gmail.com>
2025-10-13 18:52:33 -0300

    Update main02.js

--- desafio-de-projeto05 - Publicando Seu Portfólio Profissional no GitHub Pages/assets/js/main02.js
+++ desafio-de-projeto05 - Publicando Seu Portfólio Profissional no GitHub Pages/assets/js/main02.js
@@ -34,9 +34,18 @@ function updateHardSkills(profileData) {
     hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
 }
 
+/*function updateLanguages(profileData) {
+   const languages = document.getElementById('profile.languages')
+   languages.innerHTML = profileData.skills.languages.map(skills => `<li>${skills}</li>`).join('')
+}*/
+
+
+
+profile.languages
 (async () => {
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
     updateSoftSkills(profileData)
     updateHardSkills(profileData)
+    //updateLanguages(profileData)
 })()
